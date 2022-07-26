5b5ea413f7b2afc1231bec44c5cc5a19968552ff
Circl3s <maak4422@gmail.com>
2022-07-26 15:00:35 +0200

    Updated scripts.

--- scripts/generate_thumbnails.ps1
+++ scripts/generate_thumbnails.ps1
@@ -8,8 +8,8 @@ if ($Path) {
     $Episodes = Get-ChildItem -Path $Path -Recurse -Filter "*.mp4"
 
     ForEach($Episode in $Episodes) {
-        ffmpeg.exe -n -ss 00:01:00 -i $Episode.FullName -frames:v 1 -vf "scale=-1:240" $($Episode.FullName + ".jpg")
-        ffmpeg.exe -n -ss 00:01:00 -i $Episode.FullName -loop 0 -vf "select=not(mod(n\, 2880)), scale=-1:240, setpts=(1/120)*PTS" $($Episode.FullName + ".webp")
+        ffmpeg -n -ss 00:01:00 -i $Episode.FullName -frames:v 1 -vf "scale=-1:240" $($Episode.FullName + ".jpg")
+        ffmpeg -n -ss 00:01:00 -i $Episode.FullName -loop 0 -vf "select=not(mod(n\, 2880)), scale=-1:240, setpts=(1/120)*PTS" $($Episode.FullName + ".webp")
     }
 } else {
     Write-Error "You must be in the main or script directory of Helix to run this script."
--- scripts/generate_thumbnails.sh
+++ scripts/generate_thumbnails.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [[ "$PWD" = *"helix/scripts" ]]; then
-    path="../content/**/*.mp4"
+    path="../content/"
 elif [[ "$PWD" = *"helix" ]]; then
-    path="./content/**/*.mp4"
+    path="./content/"
 fi
 
 if [ -z ${path+x} ]; then
     echo "$(basename $0): You must be in the main or script directory of Helix to run this script."
     exit 1
 else
-    for f in $path
+    for f in $(find $path -name "*.mp4" -type f)
     do
         ffmpeg -n -ss 00:01:00 -i $f -frames:v 1 -vf "scale=-1:240" $f.jpg
         ffmpeg -n -ss 00:01:00 -i $f -loop 0 -vf "select=not(mod(n\, 2880)), scale=-1:240, setpts=(1/120)*PTS" $f.webp
