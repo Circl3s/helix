dcab67dcbb9e9b0f5aa33d172f8e9ab5e6a4d4ba
Circl3s <maak4422@gmail.com>
2024-03-16 17:28:14 +0000

    Quieter ffmpeg.

--- scripts/generate_thumbnails.ps1
+++ scripts/generate_thumbnails.ps1
@@ -8,8 +8,9 @@ if ($Path) {
     $Episodes = Get-ChildItem -Path $Path -Recurse -Filter "*.mp4"
 
     ForEach($Episode in $Episodes) {
-        ffmpeg -n -ss 00:01:00 -i $Episode.FullName -frames:v 1 -vf "scale=-1:240" $($Episode.FullName + ".jpg")
-        ffmpeg -n -ss 00:01:00 -i $Episode.FullName -loop 0 -vf "select=not(mod(n\, 2880)), scale=-1:240, setpts=(1/120)*PTS" $($Episode.FullName + ".webp")
+        Write-Output "${$Episode.FullName}:"
+        ffmpeg -loglevel error -stats -n -ss 00:01:00 -i $Episode.FullName -frames:v 1 -vf "scale=-1:240" $($Episode.FullName + ".jpg")
+        ffmpeg -loglevel error -stats -ss 00:01:00 -i $Episode.FullName -loop 0 -vf "select=not(mod(n\, 2880)), scale=-1:240, setpts=(1/120)*PTS" $($Episode.FullName + ".webp")
     }
 } else {
     Write-Error "You must be in the main or script directory of Helix to run this script."
--- scripts/generate_thumbnails.sh
+++ scripts/generate_thumbnails.sh
@@ -12,7 +12,8 @@ if [ -z ${path+x} ]; then
 else
     for f in $(find $path -name "*.mp4" -type f)
     do
-        ffmpeg -n -ss 00:01:00 -i $f -frames:v 1 -vf "scale=-1:240" $f.jpg
-        ffmpeg -n -ss 00:01:00 -i $f -loop 0 -vf "select=not(mod(n\, 2880)), scale=-1:240, setpts=(1/120)*PTS" $f.webp
+        echo "$f:"
+        ffmpeg -loglevel error -stats -n -ss 00:01:00 -i $f -frames:v 1 -vf "scale=-1:240" $f.jpg
+        ffmpeg -loglevel error -stats -n -ss 00:01:00 -i $f -loop 0 -vf "select=not(mod(n\, 2880)), scale=-1:240, setpts=(1/120)*PTS" $f.webp
     done
 fi
